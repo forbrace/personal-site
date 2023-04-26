c16e131af0c34b6c088597ac05c9475b2eeeb7e1
Dmitriy Paputsa <2427144+forbrace@users.noreply.github.com>
2023-04-26 22:50:20 +0100

    refactor

--- components/UI/Header.tsx
+++ components/UI/Header.tsx
@@ -83,9 +83,9 @@ const Header = () => {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white dark:bg-black px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link href="/" className="-m-1.5 p-1.5 font-bold">
+            <div className="-m-1.5 p-1.5 font-bold">
               Dima Paputsa
-            </Link>
+            </div>
             <button
               type="button"
               className="-m-2.5 rounded-md p-2.5 text-gray-700"
@@ -104,7 +104,7 @@ const Header = () => {
                 {menuItems.map((item) => (
                   <ActiveLink
                     href={item.path}
-                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50 dark:text-white dark:hover:bg-zinc-800"
+                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7  hover:bg-gray-50 dark:hover:bg-zinc-800"
                     activeClassName="text-rose-600 dark:text-rose-600"
                     onClick={() => setMobileMenuOpen(false)}
                     key={item.key}
